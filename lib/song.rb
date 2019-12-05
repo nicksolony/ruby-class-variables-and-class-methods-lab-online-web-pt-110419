09ac2fd6bd893466daeedc02f948469e439d21fe
Nick Solonyy <nicksolony@gmail.com>
2019-12-05 03:40:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,13 @@
 class Song
   
-  attr_accessor :name, :artist, :genre
-  
   def initialize (name,artist,genre)
-    
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  
+  def name
+    @name
   end
   
 end
